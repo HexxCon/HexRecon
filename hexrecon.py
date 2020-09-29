c8c7133d35695a5e1f7114154dd001c82022718d
HexxCon <hexxcon@outlook.com>
2020-09-29 21:59:29 +0100

    Updated

--- hexrecon.py
+++ hexrecon.py
@@ -3,46 +3,54 @@ import subprocess
 import argparse
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description='hexrecon')
-    parser.add_argument(
-        '-u', '--url', type=str, help='Domain URL', required=False, default=False)
-    parser.add_argument(
-        '--install', help='Install HexRecon', nargs='?', default=False)
+        parser = argparse.ArgumentParser(description='HexRecon - Please use one of the following options:')
+        parser.add_argument('-u', '--url', type=str, help='supply URL for subdomain enumeration', default=False)
+        parser.add_argument('--install', help='install required 3rd party programs', nargs='?', default=False)
 
-    return parser.parse_args()
+        return parser.parse_args()
 
 def logo():
     print("""
- _   _          ______                         
-| | | |         | ___ \                        
-| |_| | _____  _| |_/ /_____  _____ ___  _ __  
-|  _  |/ _ \ \/ /    // _ \ \/ / __/ _ \| '_ \ 
-| | | |  __/>  <| |\ \  __/>  < (_| (_) | | | |
-\_| |_/\___/_/\_\_| \_\___/_/\_\___\___/|_| |_|
+ _   _          ______
+| | | |         | ___ \
+| |_| | _____  _| |_/ /___  ___ ___  _ __
+|  _  |/ _ \ \/ /    // _ \/ __/ _ \| '_ \
+| | | |  __/>  <| |\ \  __/ (_| (_) | | | |
+\_| |_/\___/_/\_\_| \_\___|\___\___/|_| |_|
 
-============ Made by hexcon - v0.1 ===========
+========== Made by hexcon - v0.1 ==========
 """)
 
-def makedirs():
-    bpath = "$HOME/hexrecon/targets"
+def makedir(): # make directories in pwd
+        path = "output"
 
-    if not os.path.exists(bpath):
-        os.makedirs(bpath)
-    else:
-        os.system("rm -r $HOME/hexrecon")
-        os.makedirs(bpath)
+        if not os.path.exists(path):
+                os.makedirs(path)
 
-    if not os.path.exists(url):
-        os.makedirs(url)
-    else:
-        print("Could not create directory.")
+        if not os.path.exists(path + "/" + url):
+                os.makedirs(path + "/" + url)
+                print(url + " directory created.")
+        else:
+                print("Domain supplied is not valid or the folder already exists.\n")
+
+def install_tools():
+        print("Checking for system updates...")
+        os.system("sudo apt-get update -y")
+        print("Upgrading the system...")
+        os.system("sudo apt-get upgrade -y")
+
 
 if __name__ == "__main__":
-    logo()
-    args = get_args()
-    makedirs()
-    
-    url = args.url
-    install = args.install
+        logo()
+        args = get_args()
+        url = args.url
+        install = args.install
+        if url is not False:
+                makedir()
+        else:
+                print("Please select an option. Use -h for help.\n")
+        if install is not False:
+                install_tools()
+        else:
+                print("Install error.")
 
